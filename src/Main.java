5925d0a23064d0dfd96e43c1936079869d4df714
Yasith Lankesha <95087547+yasithlankesha24@users.noreply.github.com>
2023-02-11 19:47:35 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        //alex bank account
         BankAccount bankAccount = new BankAccount("Alex", 15505);
 
         bankAccount.withdraw(570);
@@ -8,5 +9,15 @@ public class Main {
 
         System.out.println(bankAccount.getOwner());
         System.out.println(bankAccount.getBalance());
+        
+        //david bank account
+        BankAccount bankAccount2 = new BankAccount("Alex", 700);
+
+        bankAccount2.withdraw(280);
+        bankAccount2.deposit(1500);
+        bankAccount2.withdraw(1000);
+
+        System.out.println(bankAccount2.getOwner());
+        System.out.println(bankAccount2.getBalance());
     }
 }
